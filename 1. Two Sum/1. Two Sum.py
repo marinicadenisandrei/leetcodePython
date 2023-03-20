8cdc510691f00a830db57bce2cdaef424ef00307
marinicadenisandrei <marinicadenisandrei@gmail.com>
2023-03-20 17:15:58 +0200

    Finish

--- 1. Two Sum/1. Two Sum.py
+++ 1. Two Sum/1. Two Sum.py
@@ -1,11 +1,13 @@
 nums = [[2,7,11,15],[3,2,4],[3,3]]
 target = [9,6,6]
-
+final = []
 
 for lists,targets in zip(nums,target):
     for i in range(len(lists)):
         for j in range(1,len(lists)):
-            if lists[i] + lists[j] == targets:
-                print(i,j)
-    print("\n")
+            if lists[i] + lists[j] == targets and i != j:
+                final.append(i)
+                final.append(j)
+    print(list(set(final)))
+    final = []
 
