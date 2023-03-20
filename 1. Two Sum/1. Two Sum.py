806a44ccf27cac4247dc5b16add62d73cc38c399
marinicadenisandrei <marinicadenisandrei@gmail.com>
2023-03-20 17:18:35 +0200

    add test message

--- 1. Two Sum/1. Two Sum.py
+++ 1. Two Sum/1. Two Sum.py
@@ -1,6 +1,7 @@
 nums = [[2,7,11,15],[3,2,4],[3,3]]
 target = [9,6,6]
 final = []
+counter = 1
 
 for lists,targets in zip(nums,target):
     for i in range(len(lists)):
@@ -8,6 +9,7 @@ for lists,targets in zip(nums,target):
             if lists[i] + lists[j] == targets and i != j:
                 final.append(i)
                 final.append(j)
-    print(list(set(final)))
+    print(f"Test {counter}: {list(set(final))}")
+    counter += 1
     final = []
 
